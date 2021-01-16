1322ae78f941b26a0592625a88590af8a16ee88a
Akhila-Reddy-stack <60685579+Akhila-Reddy-stack@users.noreply.github.com>
2021-01-16 07:09:58 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import './styles/home.scss'
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import UserHome from './Pages/User/UserHome';
 import Scan from './Pages/User/Scan';
-import UserAccountDetails from './Pages/User/UserAccountDetails';
 
 function App() {
 
@@ -16,7 +15,6 @@ function App() {
           <Switch>
             <Route path='/user/home' component={UserHome} />
             <Route path='/scan' component={Scan} />
-            <Route path='/userAccountDetails' component={UserAccountDetails} />
             <Redirect to='/user/home' component={UserHome}></Redirect>
           </Switch>
         </div>
