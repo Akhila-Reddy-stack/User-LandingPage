ac8b81637a509162ece4f26c47d4c18cc15b8a0b
Akhila-Reddy-stack <60685579+Akhila-Reddy-stack@users.noreply.github.com>
2021-01-16 09:05:18 -0800

    Add files via upload

--- src/Components/NavBar/Sidebar.js
+++ src/Components/NavBar/Sidebar.js
@@ -1,11 +1,14 @@
 import React from "react"
 import "../../styles/sidebar.scss"
-import {  Row } from 'reactstrap';
+import { Row } from 'reactstrap';
 import user from '../../Images/user/userImg.jpg';
 import scan from '../../Images/user/barcodebg.png';
 import offer from '../../Images/user/offer.png';
 import account from '../../Images/user/account.png';
-
+import MenuIcon from '@material-ui/icons/Menu';
+import headerIcon from '../../Images/user/morrisons.png'
+import CancelIcon from '@material-ui/icons/Cancel';
+import { Link } from 'react-router-dom';
 class SideNav extends React.Component {
   state = {
     state: {
@@ -49,18 +52,16 @@ class SideNav extends React.Component {
   render() {
     const { showNav } = this.state
     let navCoverStyle = { width: showNav ? "100%" : "0" }
-    let sideNavStyle = { width: showNav ? "107%" : "0" }
+    let sideNavStyle = { width: showNav ? "" : "0" }
 
     return (
       <React.Fragment >
         <div>
-        <span onClick={this.openNavClick} class="open-nav">
-         
-          <i class="fa fa-bars" aria-hidden="true"></i>
-        </span>
-        <span class="right">  <i class="fas fa-shopping-cart"></i></span>
+          <span onClick={this.openNavClick} class="open-nav">
+            <MenuIcon className="menubar" />
+            <img className="headerIcon" src={headerIcon} />
+          </span>
         </div>
-     
         <div
           onClick={this.navCoverClick}
           class="nav-cover"
@@ -70,19 +71,18 @@ class SideNav extends React.Component {
           {/* <a href="#" onClick={this.closeNavClick} class="close-nav">
             &times;
           </a> */}
-       
+          <CancelIcon className="cancelicon close-nav " onClick={this.closeNavClick} />
           <div>
             <Row>
-            <div class="card  card-header nav-item" >
-            <img src={user} class="navimg" /> <span class="nav-title user-head">Hello, User
+              <div class="card  card-header nav-item nav-icon" >
+                <img src={user} class="navimg" /> <span class="nav-title user-head">Hello, User
                </span>
               </div>
             </Row>
             <Row>
-         
               <div class="card card-list nav-item" >
-                <img src={scan} class="navimg" /> <span class="nav-title">Scan
-                 <i class="fa fa-arrow-circle-right"></i></span>
+                <img src={scan} class="navimg" /> <span class="nav-title" ><Link to="/scan" className="linkscan"> Scan</Link>
+                  <i class="fa fa-arrow-circle-right"></i></span>
               </div>
             </Row>
             <Row>
@@ -92,12 +92,12 @@ class SideNav extends React.Component {
             </Row>
             <Row>
               <div class="card  card-list nav-item">
-                <img src={offer} class="navimg" /><span class="nav-title">offer <i class="fa fa-arrow-circle-right"></i></span>
-              </div> 
+                <img src={offer} class="navimg" /><span class="nav-title">Offer <i class="fa fa-arrow-circle-right"></i></span>
+              </div>
             </Row>
             <div class="card sign-out" onClick={this.closeNavClick} >
-            <span class="logout"> <i class="fa fa-sign-out" aria-hidden="true"> </i> <span>Logout </span></span>
-               </div>
+              <span class="logout"> <i class="fa fa-sign-out" aria-hidden="true"> </i> <span class="nav-title">Logout </span></span>
+            </div>
           </div>
         </div>
         <div id="overlay" onClick={this.closeNavClick}></div>
