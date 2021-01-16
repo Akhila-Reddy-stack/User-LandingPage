a99c901b5385cb93e783468792de39dd1dcc97a7
Akhila-Reddy-stack <60685579+Akhila-Reddy-stack@users.noreply.github.com>
2021-01-16 06:47:06 -0800

    Add files via upload

--- src/Pages/User/Scan.jsx
+++ src/Pages/User/Scan.jsx
@@ -1,18 +1,23 @@
 import React, { Fragment, PureComponent } from 'react'
-import { Container, Button } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import _ from 'lodash';
 import { Link } from 'react-router-dom';
 import { withSnackbar } from 'notistack';
 import { Loader } from '../../Components/Loading/Loader';
 import QrReader from 'react-qr-reader'
-
+import { TextInput } from '../../Components/Forms/Input'
+import Modal from 'react-responsive-modal';
+import pin from '../../Images/user/pin.png';
+import 'react-responsive-modal/styles.css';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 class Scan extends PureComponent {
     constructor(props) {
         super(props)
         this.state = {
             data: [],
             loading: true,
-            result: 'No result'
+            result: 'No result',
+            open: false,
         }
     }
 
@@ -31,39 +36,116 @@ class Scan extends PureComponent {
     handleError = err => {
         console.error(err)
     }
+    onOpenModal = () => {
+        this.setState({ open: true });
+    };
 
+    onCloseModal = () => {
+        this.setState({ open: false });
+    };
+
+    
     render() {
-        const { data, loading } = this.state;
+        const { data, loading, open } = this.state;
+       
         return (
             <Fragment>
                 <>
-                    <div className="" >
-                        <Loader fullPage loading={loading} />
-                        <div class="qr-reader">
-                            <QrReader
-                                delay={300}
-                                onError={this.handleError}
-                                onScan={this.handleScan}
-                                style={{ width: '100%' }}
-                            />
-                            <p class="blinking"> Scanned Result :{this.state.result}</p>
+
+                    <div>
+
+                        <div className="webresponsive" >
+
+
+                            <Row className="secrow">
+                                <QrReader className="wqr"
+                                    delay={300}  
+                                    onError={this.handleError}
+                                    onScan={this.handleScan}
+                                    style={{ width: '100%' }}
+                                />
+                                 <p class="blinking"> Scanned Result :{this.state.result}</p>
+                                  
+                            </Row>
 
                             <div class="txtcode">
                                 SCAN A  BARCODE OR SIGNAGE QR CODE
                             </div>
+                            <Row class="">
+                                <span class="prblm">
+                                    <Button type="submit" className="prblm-btn" onClick={this.onOpenModal}>
+                                        Problem in Scanning? <ArrowForwardIcon/>
+                                    </Button>
 
-                            <div class="info" id=""> Search for the 7-Digit item ID on your product tag
-                            to continue with the purchase.
+                                </span>
+                                <span class="changebtn">
+                                    <Button type="submit" className="btn-primary changebtn-btn">
+                                        Change
+                                    </Button>
+                                    <div class="location">Location is Wrong?</div>
+                                </span>
+                            </Row>
+                        </div>
+
+                        <div className="mobileresponsive" >
+                            <Loader fullPage loading={loading} />
+                            <div class="qr-reader">
+                                <QrReader
+                                    delay={300}
+                                    onError={this.handleError}
+                                    onScan={this.handleScan}
+                                    style={{ width: '60%' ,margin:"0 auto"}}
+                                />
+                                <p class="blinking"> Scanned Result :{this.state.result}</p>
+
+                                <div class="txtcode">
+                                    SCAN A  BARCODE OR SIGNAGE QR CODE
+                            </div>
+
+                                <div class="info" id=""> Search for the 7-Digit item ID on your product tag
+                                to continue with the purchase.
                           </div>
-                            <div class="btns" >
-                                <Button type="submit" className="Cancel-btn">
-                                    <Link className="cancellink" to="/user/home" > Cancel </Link>
-                                </Button>
-                                <Button type="submit" className="Proceed-btn"> Proceed </Button>
+                                <div class="btns" >
+                                    <Button type="submit" className="Cancel-btn">
+                                        <Link className="cancellink" to="/user/home" > Cancel </Link>
+                                    </Button>
+                                    <Button type="submit" className="Proceed-btn"> Proceed </Button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </>
+                <Modal open={open} onClose={this.onCloseModal} center>
+                    <div className="marmodal">
+                        <div className="codeimg">
+                            <img src={pin} class="pin" />
+                        </div>
+                        <div className="codeimg">
+                            <div class="info" id=""> Search for the 7-Digit item ID on your product tag
+                            to continue with the purchase.
+                          </div>
+                            <div class="codeimg">
+                                <TextInput field="id" label="" placeholder="Enter 6 Digit Id" className="digitpin"
+                                    name="id" value="" />
+                            </div>
+                            <div>
+                                <Row className="codeimg">
+                                <Row md={6} className="btn-btn"> 
+                                    <Col md={6}>
+                                        <Button type="submit" className="Cancel-btn" onClick={this.onCloseModal}>
+                                            Cancel 
+                                        </Button>
+
+                                    </Col>
+                                    <Col md={6}>
+                                        <Button type="submit" className="btn-primary Proceed-btn"> Proceed </Button>
+                                    </Col>
+                                </Row>
+                                </Row>
+                            </div>
+                        </div>
+                    </div>
+                </Modal>
             </Fragment >
         );
     }
--- src/Pages/User/UserHome.jsx
+++ src/Pages/User/UserHome.jsx
@@ -8,18 +8,60 @@ import { Loader } from '../../Components/Loading/Loader';
 import OnlineShopping from '../../Images/user/shopping.png'
 import SideNav from '../../Components/NavBar/Sidebar'
 import scan from '../../Images/user/barcode.png';
+import banner1 from '../../Images/user/bannerImg1.jpg'
+import banner2 from '../../Images/user/bannerImg2.jpg'
+import banner3 from '../../Images/user/bannerImg3.jpg'
+import banner4 from '../../Images/user/bannerImg4.jpg'
+import headerIcon from '../../Images/user/morrisons.png'
+import RBCarousel from "react-bootstrap-carousel";
+import "bootstrap/dist/css/bootstrap.css";
+import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
+import { TextInput } from '../../Components/Forms/Input'
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import PersonIcon from '@material-ui/icons/Person';
+import HeadsetMicIcon from '@material-ui/icons/HeadsetMic';
+import RoomIcon from '@material-ui/icons/Room';
 
-class UserHone extends PureComponent {
+class UserHome extends PureComponent {
     constructor(props) {
         super(props)
+        this.slider = React.createRef();
         this.state = {
             data: [],
             loading: true,
+            bannerImgs: [],
+            autoplay: true,
         }
     }
 
     componentDidMount = async () => {
         setTimeout(function () { this.setState({ loading: false }); }.bind(this), 600);
+
+
+    }
+    _onSelect = (active, direction) => {
+        console.log(`active=${active} && direction=${direction}`);
+    };
+    _visiableOnSelect = (active) => {
+        console.log(`visiable onSelect active=${active}`);
+    };
+    _slideNext = () => {
+        this.slider.current.slideNext();
+    };
+    _slidePrev = () => {
+        this.slider.current.slidePrev();
+    };
+    frameLoadCarousel = () => {
+        const { bannerImgs } = this.state;
+        console.log(bannerImgs)
+
+        return _.map(bannerImgs, a =>
+        (
+            <div>
+                <img class="card-img-top image" className="bgimages" src={a} alt="Card image" />
+            </div>
+        )
+        )
     }
 
     render() {
@@ -29,36 +71,71 @@ class UserHone extends PureComponent {
                 <>
                     <Loader fullPage loading={loading} />
                     <div className="shopping-cart">
-                        <div className="icon">
+                    <div className="shopping-header">
+                        <div className="shopping-lefticon">
                             <SideNav />
+                          
+
+                            </div>
+                            <div className="shopping-righticon">
+                          <RoomIcon  className="marRight" />
+                        
+                            <PersonIcon className="marRight"/>
+                            <HeadsetMicIcon className="marRight"/>
+                             <ShoppingCartIcon  className="marRight"/><br/>
+                             <div><div class="place">Chennai</div></div>
                         </div>
-                        <br />
-                        <div className="search">
-                            <div class="topnav">
-                                <div class="search-container searchBar">
-                                    <button type="submit"><i class="fa fa-search"></i></button>
-                                    <input type="text" placeholder="Search" name="search" />
-                                </div>
-                            </div>
-                            <div class="marker">
-                                <i class="fa fa-map-marker" aria-hidden="true"></i>
-                                <div class="place">Chennai</div>
-                                <div class="place-center">DMart</div>
-                            </div>
                         </div>
                     </div>
-                    <div class="shoppingCart">
-                        <img src={OnlineShopping} class="OnlineShopping" />
-                        <div class="shoptxt">Welcome To  In Mobile CheckOut</div>
+                    <div class="">
+                        <RBCarousel
+                            animation={true}
+                            autoplay={this.state.autoplay}
+                            slideshowSpeed={9000}
+                            defaultActiveIndex={0}
+                            leftIcon={this.state.leftIcon}
+                            rightIcon={this.state.rightIcon}
+                            onSelect={this._onSelect}
+                            ref={this.slider}
+                            version={4}
+                        >
+                            <div>
+                                <div className="carousel-center">Simplify Your Shopping</div>
+                                <img className="bannerimg1" src={banner1} />
+                            </div>
+                            <div >
+                                <div className="carousel-center">Walk Through Easily</div>
+                                <img className="bannerimg2" src={banner2} />
+                            </div>
+                            <div>
+                                <div className="carousel-center">Add to Cart Easily</div>
+                                <img className="bannerimg3" src={banner3} />
+                            </div>
+                            <div>
+                                <div className="carousel-center">Scan and Pay</div>
+                                <img className="bannerimg4" src={banner4} />
+
+                            </div>
+                        </RBCarousel>
                     </div>
                     <div class="reader">
-                        <div >
+                        <div className='col-md-4'>
                             <img src={scan} class="scanimg" />
+                            <div className=''>
+                                <Button type="submit" className="Scan-QR" >
+                                    <Link className="link" to="/scan" > Scan </Link>
+                                </Button>
+                            </div>
                         </div>
-                        <div >
-                            <Button type="submit" className="Scan-QR" >
-                                <Link className="link" to="/scan" > Scan </Link>
-                            </Button>
+                        <div className='col-md-4 or'>  OR </div>
+                        <div className='col-md-4 enterinput'>
+                            <TextInput field="id" label="" placeholder="Enter 6 Digit Id" className="userdetails"
+                                name="id" value="" />
+                            <div className=''>
+                                <Button type="submit" className="Scan-QR" >
+                                    <Link className="link" to="/scan" > Enter </Link>
+                                </Button>
+                            </div>
                         </div>
                     </div>
                 </>
@@ -67,7 +144,7 @@ class UserHone extends PureComponent {
     }
 }
 
-export default withSnackbar(UserHone)
+export default withSnackbar(UserHome)
 
 
 
